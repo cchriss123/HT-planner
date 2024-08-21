fb2a338b8d18ea5f92f60546ce6d4b4ac210b789
Christofer Nyström <christofer.nystrom@iths.se>
2024-08-21 03:16:33 +0200

    Removes some unused code

--- constants/DeviceType.ts
+++ constants/DeviceType.ts
@@ -6,10 +6,3 @@ const aspectRatio = width / height;
 
 export const isTablet = aspectRatio < 1.6 && Math.min(width, height) >= 600;
 export const isPhone = !isTablet;
-
-export function getDeviceType() {
-    return {
-        isTablet,
-        isPhone,
-    };
-}
